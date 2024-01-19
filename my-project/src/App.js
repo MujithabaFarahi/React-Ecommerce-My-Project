78e16218f5743a1aef5f246dcf409cef1b383c76
Mujithaba Farahi <124652267+MujithabaFarahi@users.noreply.github.com>
2024-01-19 21:52:48 +0530

    FrontEnd Basics

---
+++ my-project/src/App.js
@@ -0,0 +1,43 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import './App.css';
+import HomePage from './Pages/HomePage';
+import ProductPage from './Pages/ProductPage';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import {LinkContainer} from 'react-router-bootstrap';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+        <div className='d-flex flex-column site-container'>
+            <header>
+                <Navbar bg="dark" varient="dark">
+                    <Container>
+                        <LinkContainer to="/">
+                            <Navbar.Brand>React Ecommerse</Navbar.Brand>
+                        </LinkContainer>
+                    </Container>
+                </Navbar>
+                {/* <Link to="/">React Ecommerce</Link> */}
+            </header>
+            <main>
+                <Container>
+                    <Routes>
+                        <Route path="/product/:slug" element={<ProductPage/>}></Route>
+                        <Route path="/" element={<HomePage/>}></Route>
+                    </Routes>
+                </Container>
+            </main>
+            <footer>
+                <div className='text-center'>
+                    All Rights Reserved
+                </div>
+            </footer>
+        </div>
+    </BrowserRouter>    
+  );
+}
+
+export default App;
---
+++ my-project/src/Pages/HomePage.js
@@ -0,0 +1,67 @@
+import { Link } from 'react-router-dom';
+// import data from '../data';
+import { useEffect, useReducer, useState } from 'react';
+import axios from 'axios';
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'FETCH_REQUEST':
+            return {...state, loading: true};
+        case 'FETCH_SUCCESS':
+            return {...state, products: action.payload, loading: false};
+        case 'FETCH_FAIL':
+            return {...state, loading: false, error: action.payload };
+        default:
+            return state;
+    }
+}
+
+function HomePage() {
+    const [{loading, error, products}, dispatch] = useReducer(reducer, {
+        products: [],
+        loading: true,
+        error: '',
+    });
+    // const[products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            dispatch ({ type: 'FETCH_REQUEST'});
+            try {
+                const result = await axios.get('/api/products');
+                dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+            } catch (err) {
+                dispatch({ type: 'FETCH_FAIL', payload: err.message });
+            }
+            // const result = await axios.get('/api/products');
+            // setProducts(result.data);
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <h1>Featured Products</h1>
+            <div className="products">
+                {products.map((product) => (
+                    <div className="product" key={product.slug}>
+                        <Link to={`/product/${product.slug}`}>
+                            <img src={product.image} alt={product.name} />
+                        </Link>
+                        <div className="product-info">
+                            <Link to={`/product/${product.slug}`}>
+                                <p>{product.name}</p>
+                            </Link>           
+                            <p>                        
+                                <strong>Rs.{product.price}</strong></p>
+                            <button>Add to Cart</button>
+                        </div>
+                    </div>
+                ))
+                }
+            </div>
+        </div>
+    );
+}
+
+export default HomePage;
