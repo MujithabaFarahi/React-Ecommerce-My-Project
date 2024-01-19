0456f4a1067d1b832aea2183d5a8b6d6c655506f
Mujithaba Farahi <124652267+MujithabaFarahi@users.noreply.github.com>
2024-01-20 00:53:58 +0530

    Bootstrap

--- my-project/src/Pages/HomePage.js
+++ my-project/src/Pages/HomePage.js
@@ -2,6 +2,10 @@ import { Link } from 'react-router-dom';
 // import data from '../data';
 import { useEffect, useReducer, useState } from 'react';
 import axios from 'axios';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Product from '../Components/Product';
+
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -43,22 +47,14 @@ function HomePage() {
         <div>
             <h1>Featured Products</h1>
             <div className="products">
-                {products.map((product) => (
-                    <div className="product" key={product.slug}>
-                        <Link to={`/product/${product.slug}`}>
-                            <img src={product.image} alt={product.name} />
-                        </Link>
-                        <div className="product-info">
-                            <Link to={`/product/${product.slug}`}>
-                                <p>{product.name}</p>
-                            </Link>           
-                            <p>                        
-                                <strong>Rs.{product.price}</strong></p>
-                            <button>Add to Cart</button>
-                        </div>
-                    </div>
-                ))
-                }
+                <Row>
+                    {products.map((product) => (
+                        <Col sm={6} md={4} lg={3} className="mb-3">
+                            <Product product={product}></Product>
+                        </Col>
+                    ))
+                    }
+                </Row>
             </div>
         </div>
     );
